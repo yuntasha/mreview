e8ef8871c51d4a46f34d6c59cf12a3019aaf8edb
yun tasha <jaljayo85@gmail.com>
2022-08-04 02:49:28 +0900

    work1

--- src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -3,6 +3,8 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
@@ -12,6 +14,9 @@ import java.util.stream.IntStream;
 
 @SpringBootTest
 public class ReviewRepositoryTests {
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Autowired
     private ReviewRepository reviewRepository;
@@ -48,5 +53,23 @@ public class ReviewRepositoryTests {
             System.out.println("\t"+movieReview.getMember().getEmail());
             System.out.println("----------------------");
         });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+
+        Long mid = 1L; //Member의 mid
+
+        Member member = Member.builder().mid(mid).build();
+
+        //기존
+//        memberRepository.deleteById(mid);
+//        reviewRepository.deleteByMember(member);
+
+        //순서 주의
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
