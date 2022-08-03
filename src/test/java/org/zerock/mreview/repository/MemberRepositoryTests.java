fd9a1963ad62ee96991ac3f42e10d678991c7728
yun tasha <jaljayo85@gmail.com>
2022-08-03 23:58:40 +0900

    list paging repository 1

--- src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -3,14 +3,20 @@ package org.zerock.mreview.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.zerock.mreview.entity.Member;
 
+import java.util.Arrays;
 import java.util.stream.IntStream;
 
 @SpringBootTest
 public class MemberRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private MovieRepository movieRepository;
     @Test
     public void insertMembers() {
         IntStream.rangeClosed(1,100).forEach(i -> {
@@ -21,4 +27,15 @@ public class MemberRepositoryTests {
             memberRepository.save(member);
         });
     }
+
+    @Test
+    public void testListPage(){
+        PageRequest pageRequest = PageRequest.of(0,10, Sort.by(Sort.Direction.DESC,
+                "mno"));
+        Page<Object[]> result = movieRepository.getListPage(pageRequest);
+        for (Object[] objects : result.getContent()) {
+            System.out.println(Arrays.toString(objects));
+        }
+    }
+
 }
