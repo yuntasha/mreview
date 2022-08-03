32efb48d3670416b878b1f8e2997fdccd1ba75f1
yun tasha <jaljayo85@gmail.com>
2022-08-03 23:22:42 +0900

    movie repository

---
+++ src/main/java/org/zerock/mreview/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package org.zerock.mreview.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.zerock.mreview.entity.Movie;
+
+
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
