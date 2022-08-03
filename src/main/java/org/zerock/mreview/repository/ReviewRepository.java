431ce20a5bc3dd72e40a05f9ea202ef70cf0e8b2
yun tasha <jaljayo85@gmail.com>
2022-08-04 02:31:44 +0900

    read paging repository 3

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package org.zerock.mreview.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.zerock.mreview.entity.Movie;
@@ -9,5 +10,6 @@ import java.util.List;
 
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     List<Review> findByMovie(Movie movie);
 }
