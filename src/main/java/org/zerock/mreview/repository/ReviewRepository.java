f6d4362907b86ef68fcd2b0f66df7b96ed17e113
yun tasha <jaljayo85@gmail.com>
2022-08-04 02:20:14 +0900

    read paging repository 2

--- src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -2,8 +2,12 @@ package org.zerock.mreview.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
+
+import java.util.List;
 
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findByMovie(Movie movie);
 }
--- src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -7,6 +7,7 @@ import org.zerock.mreview.entity.Member;
 import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
+import java.util.List;
 import java.util.stream.IntStream;
 
 @SpringBootTest
@@ -34,4 +35,18 @@ public class ReviewRepositoryTests {
         });
     }
 
+    @Test
+    public void testGetMovieReviews(){
+        Movie movie = Movie.builder().mno(92L).build();
+
+        List<Review> result = reviewRepository.findByMovie(movie);
+
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum());
+            System.out.println("\t"+movieReview.getGrade());
+            System.out.println("\t"+movieReview.getText());
+            System.out.println("\t"+movieReview.getMember().getEmail());
+            System.out.println("----------------------");
+        });
+    }
 }
