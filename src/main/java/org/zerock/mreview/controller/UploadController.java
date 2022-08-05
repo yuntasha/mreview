58735073b833ca97f9a8ec128b4a073dbfb084b6
yun tasha <jaljayo85@gmail.com>
2022-08-05 23:24:31 +0900

    uploadTest2

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -8,14 +9,16 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @Log4j2
 public class UploadController {
+    @Value("${org.zerock.upload.path}") // application.properties의 변수
+    private String uploadPath;
     @PostMapping("/uploadAjax")
     public void uploadFile(MultipartFile[] uploadFiles){
         for (MultipartFile uploadFile: uploadFiles) {
 //실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
-            String fileName =
-                    originalName.substring(originalName.lastIndexOf("\\") + 1);
+            String fileName = originalName.substring(originalName.lastIndexOf("\\") + 1);
             log.info("fileName: " + fileName);
         }//end for
     }
+
 }
