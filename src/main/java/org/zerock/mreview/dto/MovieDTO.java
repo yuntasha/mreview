260de75062ba24657a72dffad6da804ffcaabac9
yun tasha <jaljayo85@gmail.com>
2022-08-07 20:32:11 +0900

    creatDTO

---
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -0,0 +1,21 @@
+package org.zerock.mreview.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class MovieDTO {
+    private Long mno;
+    private String title;
+    @Builder.Default
+    private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+}
