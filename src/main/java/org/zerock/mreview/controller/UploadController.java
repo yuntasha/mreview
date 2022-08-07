6d7577a7c6616c40b7358b1692eb397bed8ac6fc
yun tasha <jaljayo85@gmail.com>
2022-08-07 18:59:18 +0900

    uploadByJSON

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,9 +2,12 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,6 +15,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -20,11 +25,14 @@ public class UploadController {
     @Value("${org.zerock.upload.path}") // application.properties의 변수
     private String uploadPath;
     @PostMapping("/uploadAjax")
-    public void uploadFile(MultipartFile[] uploadFiles){
+    public ResponseEntity<List<UploadResultDTO>> uploadFile(MultipartFile[] uploadFiles){
+
+        List<UploadResultDTO> resultDTOList = new ArrayList<>();
+
         for (MultipartFile uploadFile: uploadFiles) {
             if(uploadFile.getContentType().startsWith("image") == false) { //이미지파일만 업로드 가능
                 log.warn("this file is not image type");
-                return;
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
             }
 //실제 파일 이름 IE나 Edge는 전체 경로가 들어오므로
             String originalName = uploadFile.getOriginalFilename();
@@ -38,11 +46,13 @@ public class UploadController {
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid +"_" + fileName;
             Path savePath = Paths.get(saveName);
             try {
-                uploadFile.transferTo(savePath);
+                uploadFile.transferTo(savePath); //실제 이미지 저장
+                resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }//end for
+        return new ResponseEntity<>(resultDTOList, HttpStatus.OK);
     }
     private String makeFolder() {
         String str = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd"));
