d732b888bb6374007a1648639dc36c96e17cce6a
yun tasha <jaljayo85@gmail.com>
2022-08-07 19:31:22 +0900

    creatSumnail

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -1,6 +1,7 @@
 package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
+import net.coobird.thumbnailator.Thumbnailator;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -51,7 +52,15 @@ public class UploadController {
             String saveName = uploadPath + File.separator + folderPath + File.separator + uuid +"_" + fileName;
             Path savePath = Paths.get(saveName);
             try {
-                uploadFile.transferTo(savePath); //실제 이미지 저장
+//원본 파일 저장
+                uploadFile.transferTo(savePath);
+//섬네일 생성
+                String thumbnailSaveName = uploadPath + File.separator + folderPath + File.separator
+                        +"s_" + uuid +"_" + fileName;
+//섬네일 파일 이름은 중간에 s_로 시작하도록
+                File thumbnailFile = new File(thumbnailSaveName);
+//섬네일 생성
+                Thumbnailator.createThumbnail(savePath.toFile(), thumbnailFile,100,100 );
                 resultDTOList.add(new UploadResultDTO(fileName,uuid,folderPath));
             } catch (IOException e) {
                 e.printStackTrace();
