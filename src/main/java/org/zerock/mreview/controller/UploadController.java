017b6cf4bf86a89f380b7b2c95a7fc0d7f2ed39d
yun tasha <jaljayo85@gmail.com>
2022-08-07 19:16:15 +0900

    uploadImgBrowser

--- src/main/java/org/zerock/mreview/controller/UploadController.java
+++ src/main/java/org/zerock/mreview/controller/UploadController.java
@@ -2,8 +2,11 @@ package org.zerock.mreview.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +14,8 @@ import org.zerock.mreview.dto.UploadResultDTO;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URLDecoder;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDate;
@@ -65,4 +70,24 @@ public class UploadController {
         return folderPath;
     }
 
+    @GetMapping("/display")
+    public ResponseEntity<byte[]> getFile(String fileName) {
+        ResponseEntity<byte[]> result = null;
+        try {
+            String srcFileName = URLDecoder.decode(fileName,"UTF-8");
+            log.info("fileName: " + srcFileName);
+            File file = new File(uploadPath +File.separator+ srcFileName);
+            log.info("file: " + file);
+            HttpHeaders header = new HttpHeaders();
+//MIME타입 처리
+            header.add("Content-Type", Files.probeContentType(file.toPath()));
+//파일 데이터 처리
+            result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file),
+                    header, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+        return result;
+    }
 }
