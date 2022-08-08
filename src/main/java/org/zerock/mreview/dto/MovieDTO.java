47294ca5afe965abc17b152bf2a093f4122ccd2f
yun tasha <jaljayo85@gmail.com>
2022-08-08 20:27:53 +0900

    setMovieDTO

--- src/main/java/org/zerock/mreview/dto/MovieDTO.java
+++ src/main/java/org/zerock/mreview/dto/MovieDTO.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,4 +19,10 @@ public class MovieDTO {
     private String title;
     @Builder.Default
     private List<MovieImageDTO> imageDTOList = new ArrayList<>();
+    //영화의 평균 평점
+    private double avg;
+    //리뷰 수 jpa의 count( )
+    private int reviewCnt;
+    private LocalDateTime regDate;
+    private LocalDateTime modDate;
 }
