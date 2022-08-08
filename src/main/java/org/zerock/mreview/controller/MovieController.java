dc2da8634f873db38f563754361e9efee44cac8d
yun tasha <jaljayo85@gmail.com>
2022-08-08 23:09:33 +0900

    ListPage1

--- src/main/java/org/zerock/mreview/controller/MovieController.java
+++ src/main/java/org/zerock/mreview/controller/MovieController.java
@@ -3,12 +3,17 @@ package org.zerock.mreview.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.mreview.dto.MovieDTO;
+import org.zerock.mreview.dto.PageRequestDTO;
 import org.zerock.mreview.service.MovieService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/movie")
@@ -27,5 +32,11 @@ public class MovieController {
         Long mno = movieService.register(movieDTO);
         redirectAttributes.addFlashAttribute("msg", mno);
         return "redirect:/movie/list";
+    }
+
+    @GetMapping("/list")
+    public void list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("pageRequestDTO: " + pageRequestDTO);
+        model.addAttribute("result", movieService.getList(pageRequestDTO));
     }
 }
